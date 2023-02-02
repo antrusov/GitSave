edcdadc458435e12324f5dcc5874d346abceb049
ast <ast@htc-cs.com>
2023-02-02 16:52:31 +0400

    обвязка для команд

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using GitSave.Models;
 using ReactiveUI;
 
@@ -8,17 +10,58 @@ namespace GitSave.ViewModels;
 
 public class MainWindowViewModel : ReactiveObject
 {
-    //Comment
-    private string? _Comment;
-
-    public string? Comment
+    public MainWindowViewModel()
     {
-        get => _Comment;
-        set => this.RaiseAndSetIfChanged(ref _Comment, value);
+        IObservable<bool> canExecuteNewCommand =
+            this.WhenAnyValue(vm => vm.NewComment, (comment) => !string.IsNullOrEmpty(comment));
+
+        IObservable<bool> canExecuteUpdateCommand =
+            this.WhenAnyValue(vm => vm.LastComment, (comment) => !string.IsNullOrEmpty(comment));
+
+        NewCommand =
+            ReactiveCommand.Create<string?>(comment => OnNew(comment), canExecuteNewCommand);
+
+        UpdateCommand =
+            ReactiveCommand.Create<string?>(comment => OnUpdate(comment), canExecuteNewCommand);
+
+        ResetCommand = ReactiveCommand.Create(OnReset);
     }
 
-    //commits
+    #region [ Properties ]
+
+    private string? _NewComment;
+    public string? NewComment
+    {
+        get => _NewComment;
+        set => this.RaiseAndSetIfChanged(ref _NewComment, value);
+    }
+
+    private string? _LastComment;
+    public string? LastComment
+    {
+        get => _LastComment;
+        set => this.RaiseAndSetIfChanged(ref _LastComment, value);
+    }
+
     public ObservableCollection<Commit> Commits { get; } = new ObservableCollection<Commit>(GenerateMockCommitTable());
+
+    #endregion
+
+    #region [ Commands ]
+
+    public ICommand NewCommand { get; }
+    public ICommand UpdateCommand { get; }
+    public ICommand ResetCommand { get; }
+
+    #endregion
+
+    #region [ Helpers ]
+
+    public async Task OnNew(string? comment) => NewComment = "1";
+
+    public async Task OnUpdate(string? comment) => NewComment = "2";
+
+    public async Task OnReset() => NewComment = "3";
 
     static IEnumerable<Commit> GenerateMockCommitTable()
     {
@@ -332,4 +375,7 @@ public class MainWindowViewModel : ReactiveObject
 
             return defaultCommit;
     }
+
+    #endregion
+
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Git Save
+
+## Links
+* https://sharphook.tolik.io/v4.0.0/articles/hooks.html
