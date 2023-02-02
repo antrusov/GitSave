fa79bd8221f554738e8d3d7d55f4e5a67637e585
ast <ast@htc-cs.com>
2023-02-02 14:02:34 +0400

    add DataGrid

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GitSave.Models;
+using ReactiveUI;
+
+namespace GitSave.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
+{
+    public ObservableCollection<Commit> Commits { get; } = new ObservableCollection<Commit>(GenerateMockCommitTable());
+
+    static IEnumerable<Commit> GenerateMockCommitTable()
+    {
+            var defaultCommit = new List<Commit>()
+            {
+                new Commit()
+                {
+                    UUID = "123",
+                    Comment = "Comment1",
+                    Login = "Login1",
+                    Email = "Email1",
+                    Created = DateTime.Now,
+                },
+                new Commit()
+                {
+                    UUID = "456",
+                    Comment = "Comment2",
+                    Login = "Login2",
+                    Email = "Email2",
+                    Created = DateTime.Now.AddMinutes(-5),
+                },
+            };
+
+            return defaultCommit;
+    }
+}
